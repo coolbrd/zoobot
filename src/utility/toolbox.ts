c9096965158b5a8c281f9a8f149422f67a0eb28f
SkyPresident <ianthomaseviston@gmail.com>
2020-08-11 12:45:12 -0400

    Improved fidelity of getGuildUserDisplayColor

--- src/utility/toolbox.ts
+++ src/utility/toolbox.ts
@@ -1,13 +1,21 @@
-import { User, Guild } from 'discord.js';
+import { UserResolvable, GuildResolvable } from 'discord.js';
+import { client } from '..';
 
+// Does pretty much what you'd expect it to
 export function capitalizeFirstLetter(string: string) {
     return string.charAt(0).toUpperCase() + string.slice(1);
 }
 
-export function getGuildUserDisplayColor(user: User, guild: Guild) {
-    const guildMember = guild.member(user);
+// Gets a user's display color in a given guild
+export function getGuildUserDisplayColor(userResolvable: UserResolvable, guildResolvable: GuildResolvable) {
+    const guild = client.guilds.resolve(guildResolvable);
+    if (!guild) {
+        throw new Error("Attempted to get the display color of a user from a guild that could not be resolved.");
+    }
+
+    const guildMember = guild.member(userResolvable)
     if (!guildMember) {
-        return undefined;
+        throw new Error("Attempted to get the display color of a user that could not be resolved.");
     }
 
     return guildMember.displayColor;
