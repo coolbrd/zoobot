894ac6fb0c9c198d5cc2f9e37f8cf101ff0d1c22
Ian <ianthomasevistoN@gmail.com>
2020-09-04 12:24:19 -0400

    Added SpeciesInfoMessage

--- src/messages/speciesInfoMessage.ts
+++ src/messages/speciesInfoMessage.ts
@@ -1,5 +1,5 @@
 import { InteractiveMessage } from "./interactiveMessage";
-import { DMChannel, TextChannel, MessageEmbed } from "discord.js";
+import { DMChannel, TextChannel, MessageEmbed, User } from "discord.js";
 import { SpeciesObject } from "../models/species";
 import { SmartEmbed } from "../utility/smartEmbed";
 import { capitalizeFirstLetter } from "../utility/toolbox";
@@ -8,6 +8,8 @@ export class SpeciesInfoMessage extends InteractiveMessage {
     private readonly species: SpeciesObject;
 
     private currentImage = 0;
+
+    private pictureMode = true;
 
     public constructor(channel: TextChannel | DMChannel, species: SpeciesObject) {
         super(channel, { buttons: [
@@ -18,6 +20,10 @@ export class SpeciesInfoMessage extends InteractiveMessage {
             {
                 name: 'rightArrow',
                 emoji: '➡️'
+            },
+            {
+                name: 'info',
+                emoji: 'ℹ️'
             }
         ]});
 
@@ -29,10 +35,53 @@ export class SpeciesInfoMessage extends InteractiveMessage {
     private buildEmbed(): MessageEmbed {
         const newEmbed = new SmartEmbed();
 
-        newEmbed.setTitle(capitalizeFirstLetter(this.species.commonNames[0]));
-        newEmbed.setDescription(capitalizeFirstLetter(this.species.scientificName));
-        newEmbed.setImage(this.species.images[this.currentImage].url);
+        const image = this.species.images[this.currentImage];
+
+        if (this.pictureMode) {
+            newEmbed.setTitle(capitalizeFirstLetter(this.species.commonNames[0]));
+
+            newEmbed.addField('~~                 ~~', capitalizeFirstLetter(this.species.scientificName), true);
+
+            newEmbed.setImage(image.url);
+            if (image.breed) {
+                newEmbed.addField('Breed', capitalizeFirstLetter(image.breed), true);
+            }
+        }
+        else {
+            newEmbed.setTitle(capitalizeFirstLetter(this.species.scientificName));
+
+            newEmbed.setDescription(`Also known as: ${this.species.commonNames.join(', ')}`);
+
+            newEmbed.addField('Description', this.species.description);
+
+            newEmbed.addField('Habitat', this.species.naturalHabitat);
+
+            newEmbed.addField('More info', this.species.wikiPage);
+
+            newEmbed.setThumbnail(image.url);
+        }
+
+        newEmbed.setFooter(`${this.currentImage + 1}/${this.species.images.length}`);
 
         return newEmbed;
+    }
+
+    public buttonPress(buttonName: string, _user: User): void {
+        switch (buttonName) {
+            case 'rightArrow': {
+                this.currentImage = this.currentImage + 1 >= this.species.images.length ? 0 : this.currentImage + 1;
+                break;
+            }
+            case 'leftArrow': {
+                this.currentImage = this.currentImage - 1 < 0 ? this.species.images.length - 1 : this.currentImage - 1;
+                break;
+            }
+            case 'info': {
+                this.pictureMode = !this.pictureMode;
+                break;
+            }
+        }
+
+        this.setEmbed(this.buildEmbed());
     }
 }
