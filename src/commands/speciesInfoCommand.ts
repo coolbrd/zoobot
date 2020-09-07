7b5f6a5bbb087271bec4ea89903f7b3c67b11097
Ian <ianthomasevistoN@gmail.com>
2020-09-07 11:27:18 -0400

    Documented

--- src/commands/speciesInfoCommand.ts
+++ src/commands/speciesInfoCommand.ts
@@ -5,7 +5,7 @@ import { betterSend } from "../utility/toolbox";
 import { SpeciesInfoMessage } from "../messages/speciesInfoMessage";
 
 export class SpeciesInfoCommand implements Command {
-    public readonly commandNames = ['info'];
+    public readonly commandNames = ['info', 'search'];
 
     public help(commandPrefix: string): string {
         return `Use ${commandPrefix}info <species> to view a species' traits and images.`;
@@ -17,15 +17,18 @@ export class SpeciesInfoCommand implements Command {
         const fullSearchTerm = parsedUserCommand.args.join(' ').toLowerCase();
 
         const exclusions = { _id: 0 };
+        // Find a species by either its common name, or its scientific name if no common name matches were made
         const speciesDocument = await Species.findOne({ commonNamesLower: fullSearchTerm }, exclusions) || await Species.findOne({ scientificName: fullSearchTerm }, exclusions);
 
+        // If no species with the given name was found
         if (!speciesDocument) {
             betterSend(channel, `No animal by the name "${fullSearchTerm}" could be found.`);
             return;
         }
 
+        // Convert the document to a simple object
         const species = speciesDocument.toObject() as SpeciesObject;
-
+        // Construct and send an informational message about the species
         const infoMessage = new SpeciesInfoMessage(channel, species);
         infoMessage.send();
     }
--- src/messages/speciesInfoMessage.ts
+++ src/messages/speciesInfoMessage.ts
@@ -7,9 +7,9 @@ import { client } from "..";
 
 export class SpeciesInfoMessage extends InteractiveMessage {
     private readonly species: SpeciesObject;
-
+    // The current image of the species that's being displayed by the info message
     private currentImage = 0;
-
+    // Whether the info message is displaying a large image, or the species' details
     private pictureMode = true;
 
     public constructor(channel: TextChannel | DMChannel, species: SpeciesObject) {
@@ -36,20 +36,25 @@ export class SpeciesInfoMessage extends InteractiveMessage {
     private buildEmbed(): MessageEmbed {
         const newEmbed = new SmartEmbed();
 
+        // Determine the image to display
         const image = this.species.images[this.currentImage];
 
+        // Set the embed's color
         newEmbed.setColor(getGuildUserDisplayColor(client.user, this.channel));
 
+        // When the info message is showing a large picture of the species (not details)
         if (this.pictureMode) {
             newEmbed.setTitle(capitalizeFirstLetter(this.species.commonNames[0]));
 
-            newEmbed.addField('~~                 ~~', capitalizeFirstLetter(this.species.scientificName), true);
+            newEmbed.addField('―――――――――――――――――――', capitalizeFirstLetter(this.species.scientificName), true);
 
             newEmbed.setImage(image.url);
+            // Display a breed field if the current image is associated with one
             if (image.breed) {
                 newEmbed.addField('Breed', capitalizeFirstLetter(image.breed), true);
             }
         }
+        // When the info message is displaying the species' details
         else {
             newEmbed.setTitle(capitalizeFirstLetter(this.species.scientificName));
 
@@ -61,9 +66,11 @@ export class SpeciesInfoMessage extends InteractiveMessage {
 
             newEmbed.addField('More info', this.species.wikiPage);
 
+            // Use the currently selected image as a thumbnail instead
             newEmbed.setThumbnail(image.url);
         }
 
+        // Show the currently selected image's index
         newEmbed.setFooter(`${this.currentImage + 1}/${this.species.images.length}`);
 
         return newEmbed;
