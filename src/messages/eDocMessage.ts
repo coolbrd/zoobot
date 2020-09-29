ac6060015595b3d0fd70b44f98be613956ac571e
Ian <ianthomasevistoN@gmail.com>
2020-09-28 21:20:35 -0400

    Started working on EditableDocument replacement

---
+++ src/messages/eDocMessage.ts
@@ -0,0 +1,266 @@
+import { DMChannel, MessageEmbed, TextChannel, User } from "discord.js";
+import { awaitUserNextMessage } from "../discordUtility/awaitUserNextMessage";
+
+import { betterSend, safeDeleteMessage } from "../discordUtility/messageMan";
+import { SmartEmbed } from "../discordUtility/smartEmbed";
+import InteractiveMessage from "../interactiveMessage/interactiveMessage";
+import InteractiveMessageHandler from "../interactiveMessage/interactiveMessageHandler";
+import { EDoc, EDocField, getEDocTypeString } from "../structures/eDoc";
+import { PointedArray } from "../structures/pointedArray";
+import { UserError } from "../structures/userError";
+import { capitalizeFirstLetter } from "../utility/arraysAndSuch";
+
+// An interactive message containing an editable document that allows for the editing of said document
+export default class EDocMessage extends InteractiveMessage {
+    // The base document
+    private readonly eDoc: EDoc;
+
+    // The selection trail of selected nested fields
+    private readonly selectionStack: EDocField[] = [];
+
+    // An array within a document that could be selected for viewing
+    private selectedArray: PointedArray<EDocField> | undefined;
+
+    public constructor(handler: InteractiveMessageHandler, channel: TextChannel | DMChannel, eDoc: EDoc, docName?: string) {
+        super(handler, channel, { buttons: [
+            {
+                name: 'pointerUp',
+                emoji: '⬆️',
+                helpMessage: 'Pointer up'
+            },
+            {
+                name: 'pointerDown',
+                emoji: '⬇️',
+                helpMessage: 'Pointer down'
+            },
+            {
+                name: 'edit',
+                emoji: '✏️',
+                helpMessage: 'Edit'
+            },
+            {
+                name: 'back',
+                emoji: '⬅️',
+                helpMessage: 'Back'
+            }
+        ]});
+        
+        this.eDoc = eDoc;
+
+        // Create an eDoc field based on the eDoc skeleton provided
+        // This is done in here so declarations of top-level eDocs don't need to explicitly declare their type, as it's implicit
+        const topField: EDocField = {
+            info: {
+                // Use the eDoc's skeleton as the field's type
+                type: eDoc.getSkeleton(),
+                // Assign the field a friendly name
+                alias: docName || 'top document'
+            },
+            // Assign the eDoc as this field's value
+            value: this.eDoc
+        }
+
+        // Add the newly created eDoc field to the front of the selection stack, meaning it's selected
+        this.selectionStack.push(topField);
+    }
+
+    // Gets this message's currently selected field
+    private getSelection(): EDocField {
+        return this.selectionStack[this.selectionStack.length - 1];
+    }
+
+    public async build(): Promise<void> {
+        this.setEmbed(this.buildEmbed());
+    }
+
+    private buildEmbed(): MessageEmbed {
+        const selectedObject = this.getSelection();
+
+        if (!(selectedObject.value instanceof EDoc)) {
+            throw new Error('Unexpected value type selected in eDoc.');
+        }
+
+        const embed = new SmartEmbed();
+
+        if (this.selectedArray === undefined) {
+            embed.setTitle(`Now editing: __${capitalizeFirstLetter(selectedObject.info.alias as string)}__`);
+
+            // Iterate over every field in the eDoc
+            for (const [fieldName, field] of selectedObject.value.getFields()) {
+                // Label the field
+                let fieldLabel = (field.info.alias || 'anonymous field') + ': ';
+                // Draw the edit icon if the current field is the selected field
+                if (fieldName === selectedObject.value.getSelectedFieldName()) {
+                    fieldLabel += '✏️';
+                }
+
+                // The string form of this field's value
+                let valueString: string;
+                // Determine the string value based on the current field's type
+                switch (getEDocTypeString(field.info.type)) {
+                    // If the field holds a string
+                    case 'string': {
+                        if (field.value === undefined) {
+                            valueString = '*Empty*';
+                        }
+                        else {
+                            valueString = field.value as string;
+                        }
+                        break;
+                    }
+                    // If the field holds a number
+                    case 'number': {
+                        if (field.value === undefined) {
+                            valueString = '*NaN*';
+                        }
+                        else {
+                            valueString = (field.value as number).toString();
+                        }
+                        break;
+                    }
+                    // If the field holds an array
+                    case 'array': {
+                        field.value = field.value as PointedArray<EDocField>;
+
+                        if (field.value === undefined || field.value.length === 0) {
+                            valueString = '*Empty list*';
+                        }
+                        else {
+                            valueString = (field.value as unknown[]).toString();
+                        }
+                        break;
+                    }
+                    // If the field holds a nested eDoc
+                    case 'edoc': {
+                        if (field.value === undefined) {
+                            throw new Error('Value for a nested EDoc cannot be undefined. Must be initialized.');
+                        }
+                        else {
+                            field.value = field.value as EDoc;
+
+                            valueString = capitalizeFirstLetter(`${field.info.alias || 'anonymous'} document`);
+                        }
+                        break;
+                    }
+                }
+
+                embed.addField(capitalizeFirstLetter(fieldLabel), valueString);
+            }
+        }
+
+        embed.setFooter(this.getButtonHelpString());
+
+        return embed;
+    }
+
+    public async buttonPress(buttonName: string, user: User): Promise<void> {
+        const selectedObject = this.getSelection();
+
+        if (!(selectedObject.value instanceof EDoc)) {
+            throw new Error('Unexpected value type selected in eDoc.');
+        }
+
+        switch (buttonName) {
+            case 'pointerUp': {
+                if (this.selectedArray) {
+                    this.selectedArray.decrementPointer();
+                }
+                else {
+                    selectedObject.value.decrementPointer();
+                }
+                break;
+            }
+            case 'pointerDown': {
+                if (this.selectedArray) {
+                    this.selectedArray.incrementPointer();
+                }
+                else {
+                    selectedObject.value.incrementPointer();
+                }
+                break;
+            }
+            case 'edit': {
+                const selectedFieldName = selectedObject.value.getSelectedFieldName();
+                const selectedField = selectedObject.value.getSelectedField();
+
+                // Do different things depending on what kind of data is in the selected field
+                switch (getEDocTypeString(selectedField.info.type)) {
+                    // Get and assign string input
+                    case 'string': {
+                        const promptString = selectedField.info.prompt || 'Enter your input for this field:';
+                        const promptMessage = await betterSend(this.channel, promptString);
+
+                        const responseMessage = await awaitUserNextMessage(this.channel, user, 60000);
+
+                        if (responseMessage) {
+                            try {
+                                selectedObject.value.set(selectedFieldName, responseMessage.content);
+                            }
+                            catch (error) {
+                                if (error instanceof UserError) {
+                                    betterSend(this.channel, error.message, 10000);
+                                }
+                                else {
+                                    throw new Error(error.message);
+                                }
+                            }
+
+                            safeDeleteMessage(responseMessage);
+                        }
+                        safeDeleteMessage(promptMessage);
+                        break;
+                    }
+                    // Get and assign number input
+                    case 'number': {
+                        const promptString = selectedField.info.prompt || 'Enter your numeric input for this field:';
+                        const promptMessage = await betterSend(this.channel, promptString);
+
+                        const responseMessage = await awaitUserNextMessage(this.channel, user, 60000);
+
+                        if (responseMessage) {
+                            try {
+                                selectedObject.value.set(selectedFieldName, responseMessage.content);
+                            }
+                            catch (error) {
+                                if (error instanceof UserError) {
+                                    betterSend(this.channel, 'Error: ' + error.message, 10000);
+                                }
+                                else {
+                                    throw new Error(error.message);
+                                }
+                            }
+
+                            safeDeleteMessage(responseMessage);
+                        }
+
+                        safeDeleteMessage(promptMessage);
+                        break;
+                    }
+                    case 'array': {
+                        this.selectedArray = selectedField.value as PointedArray<EDocField>;
+                        break;
+                    }
+                    case 'edoc': {
+                        this.selectionStack.push(selectedField);
+                        break;
+                    }
+                }
+                break;
+            }
+            case 'back': {
+                // If the message is viewing an array, deselect it
+                if (this.selectedArray) {
+                    this.selectedArray = undefined;
+                }
+                // Otherwise, if the selection stack is more than just the base document (a nested field is selected)
+                else if (this.selectionStack.length > 1) {
+                    // Remove that field from the stack, returning to the selection before it
+                    this.selectionStack.pop();
+                }
+                break;
+            }
+        }
+
+        this.setEmbed(this.buildEmbed());
+    }
+}
---
+++ src/structures/eDoc.ts
@@ -0,0 +1,259 @@
+import { PointedArray } from "./pointedArray";
+import { UserError } from "./userError";
+
+// Acceptable values for an eDoc's declared type
+// Types that are nested eDocs are simply declared in their simple skeletal form
+// Array types are indicated by encapsulating any declared type in square brackets
+type EDocTypeHint = StringConstructor | NumberConstructor | EDocSkeleton | EDocTypeHint[];
+
+// The set of information that goes with every eDoc field
+interface EDocFieldInfo {
+    // The field's value's type
+    type: EDocTypeHint,
+    // Whether or not the value is marked as being required for completion
+    required?: boolean,
+    // The human-friendly string to display as the name of this field
+    alias?: string,
+    // The text that prompts the user for input for this field
+    prompt?: string,
+    // Whether or not this field should be hidden from plain view (usually used to store immutable data for later programatic use)
+    hidden?: boolean,
+    // Options for what constitutes valid string input, and what to do with string input
+    stringOptions?: {
+        // The string value's max length, in characters
+        maxLength?: number,
+        // The case to normalize any string input to
+        forceCase?: 'lower' | 'upper'
+    },
+    // Options for what constitutes valid number input, and what to do with number input
+    numberOptions?: {
+        // The acceptable range of number input
+        min?: number,
+        max?: number
+    },
+    // Options for array behavior
+    arrayOptions?: {
+        // The minimum number of elements required to satisfy an array that's marked as required (defaults to 1)
+        minimumLength?: number,
+        // The size of the viewable portion of the array
+        viewportSize?: number
+    }
+}
+
+// A set of eDoc field information, used in the creation of eDoc instances and nested eDocs
+export interface EDocSkeleton {
+    [fieldName: string]: EDocFieldInfo
+}
+
+// The type of values found within an eDoc instance
+export type EDocValue = undefined | string | number | EDoc | PointedArray<EDocField>;
+
+// An eDoc instance's field, contains information about the field and the value itself
+export interface EDocField {
+    info: EDocFieldInfo,
+    value: EDocValue
+}
+
+// An eDoc object that can be initialized from an eDoc skeleton
+// Takes input for fields according to their defined type and options
+export class EDoc {
+    // The eDoc's skeleton, used as the eDoc's type when it's nested in another eDoc field
+    private skeleton: EDocSkeleton;
+
+    // This eDoc's fields to be set and manipulated
+    private fields = new Map<string, EDocField>();
+
+    // This eDoc's field names, repeated in a pointed array so they can be traversed easily with a pointer
+    private fieldNames = new PointedArray<string>();
+
+    public constructor(skeleton: EDocSkeleton) {
+        this.skeleton = skeleton;
+
+        // Iterate over every field's information in the provided eDoc skeleton
+        for (const [fieldName, fieldInfo] of Object.entries(skeleton)) {
+            // Initialize a new field with the given field information
+            const newField: EDocField = {
+                info: fieldInfo,
+                value: undefined
+            }
+
+            // Initialize field values as something other than undefined, depending on the field's type
+            switch (getEDocTypeString(fieldInfo.type)) {
+                // Give array fields an empty pointed array
+                case 'array': {
+                    newField.value = new PointedArray<EDocField>();
+                    break;
+                }
+                // Give eDoc fields a new eDoc according to their type
+                case 'edoc': {
+                    fieldInfo.type = fieldInfo.type as EDocSkeleton;
+
+                    newField.value = new EDoc(fieldInfo.type);
+                    break;
+                }
+            }
+
+            // Add the new field
+            this.fields.set(fieldName, newField);
+            this.fieldNames.push(fieldName);
+        }
+    }
+
+    public getSkeleton(): EDocSkeleton {
+        return this.skeleton;
+    }
+
+    // Gets a field by its identifier
+    public getField(fieldName: string): EDocField {
+        const field = this.fields.get(fieldName);
+
+        if (!field) {
+            throw new RangeError('Attempted to get data from an EDoc field that doesn\'t exist.');
+        }
+
+        return field;
+    }
+
+    // Gets this document's field map
+    public getFields(): Map<string, EDocField> {
+        return this.fields;
+    }
+
+    // Get the document's currently selected field name
+    public getSelectedField(): EDocField {
+        const selectedField = this.fields.get(this.fieldNames.selection());
+
+        if (!selectedField) {
+            throw new Error('A field name that mapped to no field in an EDoc\'s fields was found.');
+        }
+
+        return selectedField;
+    }
+
+    public getSelectedFieldName(): string {
+        return this.fieldNames.selection();
+    }
+
+    // Sets a field based on its name
+    public set(fieldName: string, input?: string | number): void {
+        // Get the field that was specified for setting
+        const field = this.fields.get(fieldName);
+
+        if (!field) {
+            throw new RangeError('Invalid field name provided to EDoc.prototype.set.');
+        }
+
+        // Get a human-friendly name for this field, even if it doesn't have one by default
+        const fieldAlias = this.getField(fieldName).info.alias || 'field';
+
+        // Set behavior depending on the type contained within this field
+        switch (getEDocTypeString(field.info.type)) {
+            case 'string': {
+                if (!input) {
+                    throw new TypeError('Input field is required for setting a field value in an eDoc.');
+                }
+
+                if (typeof input !== 'string') {
+                    throw new TypeError('Non-string input given to an eDoc string field.')
+                }
+
+                // Handle string options if they were specified
+                if (field.info.stringOptions) {
+                    // Apply max length constraint
+                    if (field.info.stringOptions.maxLength) {
+                        if (input.length > field.info.stringOptions.maxLength) {
+                            throw new UserError(`Input for \`${fieldAlias}\` must be less than \`${field.info.stringOptions.maxLength}\` characters in length.`);
+                        }
+                    }
+
+                    // Normalize case if necessary
+                    if (field.info.stringOptions.forceCase) {
+                        switch (field.info.stringOptions.forceCase) {
+                            case 'lower': {
+                                input = input.toLowerCase();
+                                break;
+                            }
+                            case 'upper': {
+                                input = input.toUpperCase();
+                                break;
+                            }
+                            default: {
+                                throw new RangeError('Invalid value for eDoc stringOptions forceCase.');
+                            }
+                        }
+                    }
+                }
+
+                // Set the new value
+                field.value = input;
+                return;
+            }
+            case 'number': {
+                if (!input) {
+                    throw new TypeError('Input field is required for setting a field value in an eDoc.');
+                }
+
+                // Attempt to convert the input to a number
+                const inputNumber = Number(input);
+
+                if (isNaN(inputNumber)) {
+                    throw new UserError(`Input for \`${fieldAlias}\` needs to be a numeric.`);
+                }
+
+                // Handle number options if they were provided
+                if (field.info.numberOptions) {
+                    // Apply range constraints
+                    if (field.info.numberOptions.min) {
+                        if (inputNumber < field.info.numberOptions.min) {
+                            throw new UserError(`Input for \`${fieldAlias}\` must be greater than or equal to \`${field.info.numberOptions.min}\`.`);
+                        }
+                    }
+
+                    if (field.info.numberOptions.max) {
+                        if (inputNumber > field.info.numberOptions.max) {
+                            throw new UserError(`Input for \`${fieldAlias}\` must be less than or equal to \`${field.info.numberOptions.min}\`.`);
+                        }
+                    }
+                }
+
+                // Assign the new numeric value
+                field.value = inputNumber;
+                return;
+            }
+            // Don't let the set method be used with arrays and documents, that doesn't make sense!
+            case 'array':
+            case 'edoc': {
+                throw new Error('Array and eDoc fields cannot be set.');
+            }
+        }
+    }
+
+    // Pointer movement
+    public decrementPointer(): void {
+        this.fieldNames.decrementPointer();
+    }
+
+    public incrementPointer(): void {
+        this.fieldNames.incrementPointer();
+    }
+}
+
+// Reduces an eDoc type hint to a simple string
+export function getEDocTypeString(type: EDocTypeHint): 'string' | 'number' | 'array' | 'edoc' {
+    switch (type) {
+        case String: {
+            return 'string';
+        }
+        case Number: {
+            return 'number';
+        }
+        default: {
+            if (Array.isArray(type)) {
+                return 'array';
+            }
+            else {
+                return 'edoc';
+            }
+        }
+    }
+}
