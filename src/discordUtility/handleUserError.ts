61505e3760c1402efb7955cdcfe915dc75be99f5
Ian <ianthomasevistoN@gmail.com>
2020-10-01 13:46:33 -0400

    Added handleUserError function

---
+++ src/discordUtility/handleUserError.ts
@@ -0,0 +1,12 @@
+import { DMChannel, NewsChannel, TextChannel } from "discord.js";
+import { UserError } from "../structures/userError";
+import { betterSend } from "./messageMan";
+
+export default function handleUserError(channel: TextChannel | DMChannel | NewsChannel, error: Error): void {
+    if (error instanceof UserError) {
+        betterSend(channel, 'Error: ' + error.message, 10000);
+    }
+    else {
+        throw error;
+    }
+}
