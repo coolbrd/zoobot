7989b013d94740510c45f2be64e168de52ecb0b4
Ian <ianthomasevistoN@gmail.com>
2020-10-05 12:23:34 -0400

    Now uses PagedMessage

--- src/messages/inventoryMessage.ts
+++ src/messages/inventoryMessage.ts
@@ -1,19 +1,17 @@
 import { TextChannel, MessageEmbed, User, GuildMember } from "discord.js";
 
-import InteractiveMessage from "../interactiveMessage/interactiveMessage";
 import { AnimalObject } from "../models/animal";
 import { capitalizeFirstLetter } from "../utility/arraysAndSuch";
 import getGuildMember from "../discordUtility/getGuildMember";
-import loopValue from "../utility/loopValue";
 import InteractiveMessageHandler from "../interactiveMessage/interactiveMessageHandler";
 import { PlayerObject } from "../models/player";
-import PointedArray from "../structures/pointedArray";
 import SmartEmbed from "../discordUtility/smartEmbed";
 import { deleteAnimal, getPlayerObject } from "../zoo/userManagement";
 import { commandHandler } from "..";
 import buildAnimalInfo from "../embedBuilders/buildAnimalInfo";
 import buildAnimalImage from "../embedBuilders/buildAnimalImage";
 import { errorHandler } from "../structures/errorHandler";
+import PagedMessage from "./pagedMessage";
 
 // The set of states that an inventory message can be in
 enum InventoryMessageState {
@@ -23,68 +21,44 @@ enum InventoryMessageState {
     release
 }
 
-export default class InventoryMessage extends InteractiveMessage {
+export default class InventoryMessage extends PagedMessage<AnimalObject> {
+    // The current display state of the message
+    private state: InventoryMessageState;
+    
     private readonly user: User;
     protected readonly channel: TextChannel;
 
-    // The inventory of AnimalObjects to display
-    private inventory = new PointedArray<AnimalObject>();
-
-    // The page to start on
-    private page = 0;
-    private animalsPerPage = 10;
-    // The number of total pages in the inventory
-    // This has to be initialized as 0 because the page count can't be determined until the message is built
-    private pageCount = 0;
-
-    // The current display state of the message
-    private state: InventoryMessageState;
-
-    private readonly guildMember: GuildMember;
     private playerObject: PlayerObject | undefined;
 
     constructor(handler: InteractiveMessageHandler, channel: TextChannel, user: User) {
-        super(handler, channel, { buttons: [
-            {
-                name: 'leftArrow',
-                emoji: '⬅️',
-                helpMessage: 'page left'
-            },
-            {
-                name: 'rightArrow',
-                emoji: '➡️',
-                helpMessage: 'page right'
-            },
-            {
-                name: 'upArrow',
-                emoji: '⬆️',
-                helpMessage: 'pointer up'
-            },
-            {
-                name: 'downArrow',
-                emoji: '⬇️',
-                helpMessage: 'pointer down'
-            },
-            {
-                name: 'mode',
-                emoji: 'Ⓜ️',
-                helpMessage: 'view mode'
-            },
-            {
-                name: 'release',
-                emoji: '🗑️',
-                helpMessage: 'release'
-            }
-        ]});
+        super(handler, channel);
+
+        this.addButtons([{
+            name: 'upArrow',
+            emoji: '⬆️',
+            helpMessage: 'Pointer up'
+        },
+        {
+            name: 'downArrow',
+            emoji: '⬇️',
+            helpMessage: 'Pointer down'
+        },
+        {
+            name: 'mode',
+            emoji: 'Ⓜ️',
+            helpMessage: 'View mode'
+        },
+        {
+            name: 'release',
+            emoji: '🗑️',
+            helpMessage: 'Release'
+        }]);
 
         this.user = user;
         this.channel = channel;
 
         // Start the inventory message in paged view mode
         this.state = InventoryMessageState.page;
-
-        // Find the Discord guild member corresponding to this message's requester
-        this.guildMember = getGuildMember(user, channel.guild);
     }
 
     // Pre-send build logic
@@ -94,20 +68,18 @@ export default class InventoryMessage extends InteractiveMessage {
         // Attempt to get the user's player object
         let playerObject: PlayerObject;
         try {
-            playerObject = await getPlayerObject(this.guildMember);
+            playerObject = await getPlayerObject(getGuildMember(this.user, this.channel.guild));
             await playerObject.load();
         }
         catch (error) {
-            errorHandler.handleError(error, 'There was an error trying to get a player object in an inventory message.');
+            errorHandler.handleError(error, 'There was an error trying to get and load a player object in an inventory message.');
             return;
         }
 
         // Assign the new player object
         this.playerObject = playerObject;
-        this.inventory = new PointedArray(this.playerObject.getAnimals());
-
-        // Calculate and set page count
-        this.pageCount = Math.ceil(this.inventory.length / this.animalsPerPage);
+        // Assign the elements that will be displayed in this message
+        this.setElements(this.playerObject.getAnimals());
 
         // Build the initial embed
         try {
@@ -124,29 +96,20 @@ export default class InventoryMessage extends InteractiveMessage {
     private async buildEmbed(): Promise<MessageEmbed> {
         const embed = new SmartEmbed();
 
-        if (this.state === InventoryMessageState.release) {
-            this.setButtonHelpMessage('leftArrow', 'confirm release');
-        }
-        else {
-            this.setButtonHelpMessage('leftArrow', 'page left');
-        }
+        const inventory = this.getElements();
 
         const userAvatar = this.user.avatarURL() || undefined;
         embed.setAuthor(`${this.user.username}'s collection`, userAvatar);
-        embed.setFooter(`${this.inventory.length} in collection\n${this.getButtonHelpString()}`);
+        embed.setFooter(`${inventory.length} in collection\n${this.getButtonHelpString()}`);
 
         // Don't try anything crazy if the user's inventory is empty
-        if (this.inventory.length < 1) {
+        if (inventory.length < 1) {
             embed.setDescription(`It's empty in here. Try catching an animal with \`${commandHandler.getGuildPrefix(this.channel.guild)}encounter\`!`);
             return embed;
         }
 
-        // Calculate the start and end points of the current page
-        const startIndex = this.page * this.animalsPerPage;
-        const endIndex = startIndex + this.animalsPerPage;
-
         // Filter the currently displayed slice of the inventory array for animals that haven't been loaded yet
-        const unloadedAnimals: AnimalObject[] = this.inventory.slice(startIndex, endIndex).filter((animalObject: AnimalObject) => {
+        const unloadedAnimals = this.getVisibleElements().filter((animalObject: AnimalObject) => {
             return !animalObject.fullyLoaded();
         });
         
@@ -168,23 +131,21 @@ export default class InventoryMessage extends InteractiveMessage {
         });
 
         // Get the animal that's selected by the pointer
-        const selectedAnimal = this.inventory.selection();
+        const selectedAnimal = inventory.selection();
         const image = selectedAnimal.getImage();
 
         // Display state behavior
         switch (this.state) {
             // When the message is in paged view mode
             case InventoryMessageState.page: {
+                // Show the selected animal's image in the thumbnail
                 embed.setThumbnail(image.getUrl());
 
+                // The string that will hold the formatted inventory string
                 let inventoryString = '';
-                // Start the current page's display at the appropriate position
-                let inventoryIndex = startIndex;
-                // Loop until either the index is above the entries per page limit or the length of the inventory
-                while (inventoryIndex < endIndex && inventoryIndex < this.inventory.length) {
-                    // Get the currently iterated animal in the user's inventory
-                    const currentAnimal: AnimalObject = this.inventory[inventoryIndex];
-
+                let inventoryIndex = this.getFirstVisibleIndex();
+                // Iterate over every element on the current page
+                this.getVisibleElements().forEach(currentAnimal => {
                     const image = currentAnimal.getImage();
 
                     const animalName = currentAnimal.getNickname() || capitalizeFirstLetter(currentAnimal.getName());
@@ -194,14 +155,14 @@ export default class InventoryMessage extends InteractiveMessage {
                     const breedText = !currentAnimal.getNickname() && breed ? `(${breed})` : '';
 
                     // The pointer text to draw on the current animal entry (if any)
-                    const pointerText = inventoryIndex === this.inventory.getPointerPosition() ? ' 🔹' : '';
+                    const pointerText = inventoryIndex === inventory.getPointerPosition() ? ' 🔹' : '';
 
                     inventoryString += `\`${inventoryIndex + 1})\` ${animalName} ${breedText}`;
 
                     inventoryString += ` ${pointerText}\n`;
 
                     inventoryIndex++;
-                }
+                });
 
                 embed.setDescription(inventoryString);
 
@@ -211,7 +172,7 @@ export default class InventoryMessage extends InteractiveMessage {
             case InventoryMessageState.info: {
                 buildAnimalInfo(embed, selectedAnimal);
 
-                embed.setTitle(`\`${this.inventory.getPointerPosition() + 1})\` ${embed.title}`);
+                embed.setTitle(`\`${inventory.getPointerPosition() + 1})\` ${embed.title}`);
                 
                 break;
             }
@@ -219,7 +180,7 @@ export default class InventoryMessage extends InteractiveMessage {
             case InventoryMessageState.image: {
                 buildAnimalImage(embed, selectedAnimal);
 
-                embed.setTitle(`\`${this.inventory.getPointerPosition() + 1})\` ${embed.title}`);
+                embed.setTitle(`\`${inventory.getPointerPosition() + 1})\` ${embed.title}`);
 
                 break;
             }
@@ -230,51 +191,30 @@ export default class InventoryMessage extends InteractiveMessage {
                 embed.setDescription(`Press the left arrow (${this.getButtonByName('leftArrow').emoji}) to confirm this release. Press any other button or do nothing to cancel.`);
 
                 embed.setThumbnail(image.getUrl());
+
+                break;
+            }
+        }
+
+        // Update button messages according to behavior
+        switch (this.state) {
+            case InventoryMessageState.release: {
+                this.setButtonHelpMessage('leftArrow', 'Confirm release');
+                break;
+            }
+            default: {
+                this.setButtonHelpMessage('leftArrow', 'Page left');
+                break;
             }
         }
 
         return embed;
     }
 
-    // Move a number of pages
-    private movePages(count: number): void {
-        // Moves the desired number of pages, looping if necessary
-        this.page = loopValue(this.page + count, 0, this.pageCount - 1);
-        // If the page move caused the pointer to be off the page
-        if (!this.pointerIsOnPage()) {
-            // Move the pointer to the first entry on the page
-            this.inventory.setPointerPosition(this.page * this.animalsPerPage);
-        }
-    }
-
-    // Gets the page that the pointer is currently on
-    private getPointerPage(): number {
-        return Math.floor(this.inventory.getPointerPosition() / this.animalsPerPage);
-    }
-
-    // Checks if the pointer is on the message's currently displayed page
-    private pointerIsOnPage(): boolean {
-        return this.page === this.getPointerPage();
-    }
-
-    // Moves to the page that the pointer is on
-    private goToPointerPage(): void {
-        this.page = this.getPointerPage();
-    }
-
-    // Moves the pointer a number of positions
-    private movePointer(count: number): void {
-        // Move the pointer to the new position
-        this.inventory.movePointer(count);
-        // If moving the pointer made it leave the page
-        if (!this.pointerIsOnPage()) {
-            // Change the page to the one that the pointer's on
-            this.goToPointerPage();
-        }
-    }
-
     public async buttonPress(buttonName: string, user: User): Promise<void> {
         super.buttonPress(buttonName, user);
+
+        const inventory = this.getElements();
 
         // If the message in any state other than releasing an animal
         if (this.state !== InventoryMessageState.release) {
@@ -331,7 +271,7 @@ export default class InventoryMessage extends InteractiveMessage {
             // If the confirmation button is pressed
             if (buttonName === 'leftArrow') {
                 // Get the selected animal that will be released
-                const selectedAnimal = this.inventory.selection();
+                const selectedAnimal = inventory.selection();
 
                 // Release the user's animal
                 try {
@@ -343,7 +283,7 @@ export default class InventoryMessage extends InteractiveMessage {
                 }
 
                 // Delete the animal from the inventory message
-                this.inventory.deleteAtPointer();
+                inventory.deleteAtPointer();
 
                 // Put the message back in paged mode
                 this.state = InventoryMessageState.page;
