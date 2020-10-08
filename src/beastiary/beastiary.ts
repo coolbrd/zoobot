929be252fe769720e3b6431a22186612f907cc32
Ian <ianthomasevistoN@gmail.com>
2020-10-08 12:57:39 -0400

    Deleted repeat lowercase files (thank you github)

--- src/beastiary/beastiary.ts
+++
@@ -1,7 +0,0 @@
-import PlayerCache from "./PlayerCache";
-
-// The central cache holder/manager for all game objects within The Beastiary
-class Beastiary {
-    public readonly players = new PlayerCache();
-}
-export const beastiary = new Beastiary();
