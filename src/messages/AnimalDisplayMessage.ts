81eae10b73d8e8556352725732d6a75fe22c342b
Ian <ianthomasevistoN@gmail.com>
2020-10-26 22:17:05 -0400

    Made AnimalDisplayMessage abstract message class for easier animal display

---
+++ src/messages/AnimalDisplayMessage.ts
@@ -0,0 +1,221 @@
+import { MessageEmbed, TextChannel, User } from "discord.js";
+import { Animal } from "../models/Animal";
+import { capitalizeFirstLetter } from "../utility/arraysAndSuch";
+import PagedMessage from "./PagedMessage";
+import { beastiary } from "../beastiary/Beastiary";
+import { Types } from "mongoose";
+import SmartEmbed from "../discordUtility/SmartEmbed";
+import buildAnimalInfo from "../embedBuilders/buildAnimalInfo";
+import buildAnimalCard from "../embedBuilders/buildAnimalCard";
+
+// The set of states that an animal display message can be in
+export enum AnimalDisplayMessageState {
+    page,
+    info,
+    card
+}
+
+// An animal id with its optionally loaded animal object
+interface LoadableAnimal {
+    id: Types.ObjectId,
+    animal?: Animal
+}
+
+// The message displaying a player's animal collection
+export default abstract class AnimalDisplayMessage extends PagedMessage<LoadableAnimal> {
+    // The current display state of the message
+    protected state: AnimalDisplayMessageState;
+
+    constructor(channel: TextChannel, animalIds: Types.ObjectId[]) {
+        super(channel);
+
+        this.addButtons([
+            {
+                name: "upArrow",
+                emoji: "⬆️",
+                helpMessage: "Pointer up"
+            },
+            {
+                name: "downArrow",
+                emoji: "⬇️",
+                helpMessage: "Pointer down"
+            },
+            {
+                name: "mode",
+                emoji: "Ⓜ️",
+                helpMessage: "View mode"
+            }
+        ]);
+
+        // Start the message in paged view mode
+        this.state = AnimalDisplayMessageState.page;
+
+        const loadableAnimals: LoadableAnimal[] = [];
+
+        for (const animalId of animalIds) {
+            loadableAnimals.push({
+                id: animalId
+            });
+        }
+
+        // Set paged elements
+        this.setElements(loadableAnimals);
+    }
+
+    protected async buildEmbed(): Promise<MessageEmbed> {
+        const embed = new SmartEmbed();
+
+        const loadableAnimalsOnPage = this.visibleElements;
+
+        if (loadableAnimalsOnPage.length <= 0) {
+            return embed;
+        }
+
+        // Bulk load all animals on the current page
+        try {
+            await new Promise(resolve => {
+                let count = 0;
+                // Iterate over every loadable animal on the page
+                loadableAnimalsOnPage.forEach(loadableAnimal => {
+                    // Get each animal's object by their id
+                    beastiary.animals.fetchById(loadableAnimal.id).then(animal => {
+                        // Assign the loaded animal
+                        loadableAnimal.animal = animal;
+
+                        // Resolve once all animals on the page are loaded
+                        if (++count >= loadableAnimalsOnPage.length) {
+                            resolve();
+                        }
+                    }).catch(error => {
+                        throw new Error(`There was an error fetching an animal in an collection message: ${error}`);
+                    });
+                });
+            });
+        }
+        catch (error) {
+            throw new Error(`There was an error bulk fetching an collection page of animals: ${error}`);
+        }
+
+        const selectedLoadableAnimal = this.elements.selection;
+
+        if (!selectedLoadableAnimal.animal) {
+            throw new Error("The selected animal within an animal display message wasn't loaded somehow.");
+        }
+
+        const selectedAnimal = selectedLoadableAnimal.animal;
+
+        switch (this.state) {
+            case AnimalDisplayMessageState.page: {
+                embed.setThumbnail(selectedAnimal.card.url);
+
+                let pageString = "";
+                let elementIndex = this.firstVisibleIndex;
+                // Iterate over every animal on the current page
+                loadableAnimalsOnPage.forEach(loadableAnimal => {
+                    // Get the animal object
+                    const currentAnimal = loadableAnimal.animal;
+
+                    if (!currentAnimal) {
+                        throw new Error("An unloaded animal that should have been loaded was found in an animal display message.");
+                    }
+
+                    const card = currentAnimal.card;
+
+                    const animalName = currentAnimal.nickname || capitalizeFirstLetter(currentAnimal.name);
+
+                    // Add special or breed text if applicable
+                    let specialText = "";
+                    if (!currentAnimal.nickname) {
+                        if (card.special) {
+                            specialText = card.special;
+                        }
+                        else if (card.breed) {
+                            specialText = card.breed;
+                        }
+                        if (specialText) {
+                            specialText = ` (${specialText})`;
+                        }
+                    }
+
+                    // The pointer text to draw on the current animal entry (if any)
+                    const pointerText = elementIndex === this.elements.pointerPosition ? " 🔹" : "";
+
+                    pageString += `\`${elementIndex + 1})\` ${animalName}${specialText}`;
+
+                    pageString += ` ${pointerText}\n`;
+
+                    elementIndex++;
+                });
+
+                embed.setDescription(pageString);
+                break;
+            }
+            case AnimalDisplayMessageState.info: {
+                buildAnimalInfo(embed, selectedAnimal);
+                break;
+            }
+            case AnimalDisplayMessageState.card: {
+                buildAnimalCard(embed, selectedAnimal);
+                break;
+            }
+        }
+
+        return embed;
+    }
+
+    public async buttonPress(buttonName: string, user: User): Promise<void> {
+        super.buttonPress(buttonName, user);
+
+        // Button behavior
+        switch (buttonName) {
+            case "upArrow": {
+                this.movePointer(-1);
+                break;
+            }
+            case "downArrow": {
+                this.movePointer(1);
+                break;
+            }
+            case "leftArrow": {
+                // Change pages if the message is in page mode, otherwise move the pointer
+                if (this.state === AnimalDisplayMessageState.page) {
+                    this.movePages(-1);
+                }
+                else {
+                    this.movePointer(-1);
+                }
+                break;
+            }
+            case "rightArrow": {
+                // Change pages if the message is in page mode, otherwise move the pointer
+                if (this.state === AnimalDisplayMessageState.page) {
+                    this.movePages(1);
+                }
+                else {
+                    this.movePointer(1);
+                }
+                break;
+            }
+            case "mode": {
+                if (this.state === AnimalDisplayMessageState.page) {
+                    this.state = AnimalDisplayMessageState.info;
+                }
+                else if (this.state === AnimalDisplayMessageState.info) {
+                    this.state = AnimalDisplayMessageState.card;
+                }
+                else {
+                    this.state = AnimalDisplayMessageState.page;
+                }
+                break;
+            }
+        }
+
+        // Rebuild the message
+        try {
+            await this.refreshEmbed();
+        }
+        catch (error) {
+            throw new Error(`There was an error rebuilding the embed of a collection message: ${error}`);
+        }
+    }
+}
--- src/messages/CollectionMessage.ts
+++ src/messages/CollectionMessage.ts
@@ -1,87 +1,33 @@
-import { TextChannel, MessageEmbed, User } from "discord.js";
-import { Animal } from "../models/Animal";
-import { capitalizeFirstLetter } from "../utility/arraysAndSuch";
+import { TextChannel, MessageEmbed } from "discord.js";
 import { Player } from "../models/Player";
-import SmartEmbed from "../discordUtility/SmartEmbed";
-import buildAnimalInfo from "../embedBuilders/buildAnimalInfo";
-import buildAnimalCard from "../embedBuilders/buildAnimalCard";
-import PagedMessage from "./PagedMessage";
 import { commandHandler } from "../structures/CommandHandler";
-import { beastiary } from "../beastiary/Beastiary";
-import { Types } from "mongoose";
-
-// The set of states that a collection message can be in
-enum CollectionMessageState {
-    page,
-    info,
-    card
-}
-
-// An animal id with its optionally loaded animal object
-interface LoadableAnimal {
-    id: Types.ObjectId,
-    animal?: Animal
-}
+import AnimalDisplayMessage, { AnimalDisplayMessageState } from "./AnimalDisplayMessage";
 
 // The message displaying a player's animal collection
-export default class CollectionMessage extends PagedMessage<LoadableAnimal> {
+export default class CollectionMessage extends AnimalDisplayMessage {
     protected readonly lifetime = 90000;
 
     protected readonly elementsPerPage = 10;
-
-    // The current display state of the message
-    private state: CollectionMessageState;
     
     private readonly player: Player;
     public readonly channel: TextChannel;
 
     constructor(channel: TextChannel, player: Player) {
-        super(channel);
-
-        this.addButtons([
-            {
-                name: "upArrow",
-                emoji: "⬆️",
-                helpMessage: "Pointer up"
-            },
-            {
-                name: "downArrow",
-                emoji: "⬇️",
-                helpMessage: "Pointer down"
-            },
-            {
-                name: "mode",
-                emoji: "Ⓜ️",
-                helpMessage: "View mode"
-            }
-        ]);
+        super(channel, player.animalIds);
 
         this.player = player;
         this.channel = channel;
-
-        // Start the collection message in paged view mode
-        this.state = CollectionMessageState.page;
-    }
-
-    // Pre-send build logic
-    public async build(): Promise<void> {
-        // The list of animals that will need to be loaded by their ids
-        const loadableAnimals: LoadableAnimal[] = [];
-
-        // Add the all the player's animal ids to the list of loadable animals
-        for (const animalId of this.player.animalIds) {
-            loadableAnimals.push({
-                id: animalId
-            });
-        }
-
-        // Set paged elements
-        this.setElements(loadableAnimals);
     }
 
     // Builds the current page of the collection's embed
     protected async buildEmbed(): Promise<MessageEmbed> {
-        const embed = new SmartEmbed();
+        let embed: MessageEmbed;
+        try {
+            embed = await super.buildEmbed();
+        }
+        catch (error) {
+            throw new Error(`There was an error building a collection message's inherited embed information: ${error}`);
+        }
 
         // Make it more clear what we're working with here
         const collection = this.elements;
@@ -95,157 +41,11 @@ export default class CollectionMessage extends PagedMessage<LoadableAnimal> {
             embed.setDescription(`It's empty in here. Try catching an animal with \`${commandHandler.getGuildPrefix(this.channel.guild)}encounter\`!`);
             return embed;
         }
-        
-        // Bulk load all animals on the current page
-        try {
-            await new Promise(resolve => {
-                let count = 0;
-                // Iterate over every loadable animal on the page
-                this.visibleElements.forEach(loadableAnimal => {
-                    // Get each animal's object by their id
-                    beastiary.animals.fetchById(loadableAnimal.id).then(animal => {
-                        // Assign the loaded animal
-                        loadableAnimal.animal = animal;
 
-                        // Resolve once all animals on the page are loaded
-                        if (++count >= this.visibleElements.length) {
-                            resolve();
-                        }
-                    }).catch(error => {
-                        throw new Error(`There was an error fetching an animal in an collection message: ${error}`);
-                    });
-                });
-            });
-        }
-        catch (error) {
-            throw new Error(`There was an error bulk fetching an collection page of animals: ${error}`);
-        }
-
-        // Get the animal that's selected by the pointer
-        const selectedAnimal = collection.selection.animal as Animal;
-        const card = selectedAnimal.card;
-
-        // Display state behavior
-        switch (this.state) {
-            // When the message is in paged view mode
-            case CollectionMessageState.page: {
-                // Show the selected animal's card in the thumbnail
-                embed.setThumbnail(card.url);
-
-                // The string that will hold the formatted collection string
-                let collectionString = "";
-                let collectionIndex = this.firstVisibleIndex;
-                // Iterate over every animal on the current page
-                this.visibleElements.forEach(loadableAnimal => {
-                    // Get the animal object (and assume it's an animal because we fetched it)
-                    const currentAnimal = loadableAnimal.animal as Animal;
-
-                    const card = currentAnimal.card;
-
-                    const animalName = currentAnimal.nickname || capitalizeFirstLetter(currentAnimal.name);
-
-                    // Add special or breed text if applicable
-                    let specialText = "";
-                    if (!currentAnimal.nickname) {
-                        if (card.special) {
-                            specialText = card.special;
-                        }
-                        else if (card.breed) {
-                            specialText = card.breed;
-                        }
-                        if (specialText) {
-                            specialText = ` (${specialText})`;
-                        }
-                    }
-
-                    // The pointer text to draw on the current animal entry (if any)
-                    const pointerText = collectionIndex === collection.pointerPosition ? " 🔹" : "";
-
-                    collectionString += `\`${collectionIndex + 1})\` ${animalName}${specialText}`;
-
-                    collectionString += ` ${pointerText}\n`;
-
-                    collectionIndex++;
-                });
-
-                embed.setDescription(collectionString);
-
-                break;
-            }
-            // When the message is in info mode
-            case CollectionMessageState.info: {
-                buildAnimalInfo(embed, selectedAnimal);
-
-                embed.setTitle(`\`${collection.pointerPosition + 1})\` ${embed.title}`);
-                
-                break;
-            }
-            // When the message is in card mode
-            case CollectionMessageState.card: {
-                buildAnimalCard(embed, selectedAnimal);
-
-                embed.setTitle(`\`${collection.pointerPosition + 1})\` ${embed.title}`);
-
-                break;
-            }
+        if (this.state === AnimalDisplayMessageState.info || this.state === AnimalDisplayMessageState.card) {
+            embed.setTitle(`\`${collection.pointerPosition + 1})\` ${embed.title}`);
         }
 
         return embed;
-    }
-
-    public async buttonPress(buttonName: string, user: User): Promise<void> {
-        super.buttonPress(buttonName, user);
-
-        // Button behavior
-        switch (buttonName) {
-            case "upArrow": {
-                this.movePointer(-1);
-                break;
-            }
-            case "downArrow": {
-                this.movePointer(1);
-                break;
-            }
-            case "leftArrow": {
-                // Change pages if the message is in page mode, otherwise move the pointer
-                if (this.state === CollectionMessageState.page) {
-                    this.movePages(-1);
-                }
-                else {
-                    this.movePointer(-1);
-                }
-                break;
-            }
-            case "rightArrow": {
-                // Change pages if the message is in page mode, otherwise move the pointer
-                if (this.state === CollectionMessageState.page) {
-                    this.movePages(1);
-                }
-                else {
-                    this.movePointer(1);
-                }
-                break;
-            }
-            case "mode": {
-                if (this.state === CollectionMessageState.page) {
-                    this.state = CollectionMessageState.info;
-                }
-                else if (this.state === CollectionMessageState.info) {
-                    this.state = CollectionMessageState.card;
-                }
-                else {
-                    this.state = CollectionMessageState.page;
-                }
-                break;
-            }
-        }
-
-        // Rebuild the message
-        try {
-            await this.refreshEmbed();
-        }
-        catch (error) {
-            throw new Error(`There was an error refreshing the embed of a collection message: ${error}`);
-        }
     }
 }
