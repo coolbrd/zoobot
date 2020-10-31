db3e6f9cff88d6a835f2f06946a39b160a3f83de
Ian <ianthomasevistoN@gmail.com>
2020-10-31 11:53:34 -0400

    Refactored and cleaned

--- src/index.ts
+++ src/index.ts
@@ -7,10 +7,8 @@ import { encounterHandler } from "./beastiary/EncounterHandler";
 import { commandHandler } from "./structures/CommandHandler";
 import { beastiary } from "./beastiary/Beastiary";
 
-// Create a new client for the bot to use
 export const client = new Discord.Client();
 
-// Flags for the bot's current initialization state
 const preLoad = {
     discordLoaded: false,
     databaseLoaded: false,
@@ -20,52 +18,45 @@ const preLoad = {
 }
 let readyForInput = false;
 
-// Called whenever the bot completes a stage of initialization
 function complete() {
-    // If all steps of initialization are complete
     if (Object.values(preLoad).every(requirement => requirement)) {
-        // Allow the bot to receive input
         readyForInput = true;
         console.log("Ready for input");
     }
 }
 
-// Connect to the MongoDB database
 mongoose.connect(MONGODB_PATH, { dbName: "zoobot", useNewUrlParser: true, useUnifiedTopology: true }).then(() => {
     console.log("MongoDB connected");
-    // Indicate that the bot has logged into the database
     preLoad.databaseLoaded = true;
     complete();
 
-    // Load all custom guild prefixes
     commandHandler.loadGuildPrefixes().then(() => {
         console.log("Guild prefixes loaded");
 
         preLoad.guildPrefixesLoaded = true;
         complete();
+    }).catch(error => {
+        throw new Error(`There was an error loading all guild prefixes: ${error}`);
     });
 
-    // Now try to load the species rarity table
     encounterHandler.loadRarityTable().then(() => {
         console.log("Rarity table loaded");
     
         preLoad.rarityTableLoaded = true;
         complete();
+    }).catch(error => {
+        throw new Error(`There was an error loading the species rarity table: ${error}`);
     });
 }).catch(error => {
-    // If there was an error connecting
-    console.error("MongoDB connection error: ", error)
+    throw new Error(`MongoDB connection error: ${error}`);
 });
 
-// When the bot is ready to receive input
 client.on("ready", () => {
     console.log("Logged into Discord");
 
-    // Indicate that the bot has logged into Discord
     preLoad.discordLoaded = true;
     complete();
 
-    // Initialize all centralized handlers
     errorHandler.init(client).then(() => {
         interactiveMessageHandler.init(client);
 
@@ -82,27 +73,24 @@ client.on("ready", () => {
     });
 });
 
-// When the bot receives a message
 client.on("message", (message: Message) => {
-    // If the bot is not yet ready to receive input
     if (!readyForInput) {
-        // Don't do anything with the message
         return;
     }
 
-    // Handle the incoming message
     commandHandler.handleMessage(message).then(() => {
-        beastiary.players.handleMessage(message);
+        beastiary.players.handleMessage(message).catch(error => {
+            throw new Error(`There was an error handling a message through the player manager: ${error}`);
+        });
+    }).catch(error => {
+        throw new Error(`There was an error handling a message as a command: ${error}`);
     });
 });
 
-// When the bot encounters an error
 client.on("error", error => console.error("Discord client error: ", error));
 
-// Log the bot into the discord client with the provided token
 client.login(DISCORD_TOKEN);
 
-// Gracefully shuts the bot down
 export async function exit(): Promise<void> {
     client.destroy();
 
