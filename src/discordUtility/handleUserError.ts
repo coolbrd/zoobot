c07e7b149e8bb093572cc83e33d932ab12a7d349
Ian <ianthomasevistoN@gmail.com>
2020-11-05 13:51:28 -0500

    Removed user error prefix

--- src/discordUtility/handleUserError.ts
+++ src/discordUtility/handleUserError.ts
@@ -5,7 +5,7 @@ import { betterSend } from "./messageMan";
 // Handles an error that could potentially be a user error, requiring display to the user
 export default function handleUserError(channel: TextChannel | DMChannel, error: Error, lifetime?: number): Error | undefined {
     if (error instanceof UserError) {
-        betterSend(channel, "Error: " + error.message, lifetime);
+        betterSend(channel, error.message, lifetime);
         return;
     }
     else {
