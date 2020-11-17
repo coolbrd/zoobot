78ff48215016b550a0997042a14522e98a659a04
Ian <ianthomasevistoN@gmail.com>
2020-11-17 13:34:39 -0500

    Reduced shard count

--- src/sharding.ts
+++ src/sharding.ts
@@ -1,5 +1,5 @@
 import Discord from "discord.js";
 const Manager = new Discord.ShardingManager("./build/index.js");
 
-Manager.spawn(3);
+Manager.spawn(1);
 Manager.on("shardCreate", shard => console.log(`- Spawned shard ${shard.id} -`));
