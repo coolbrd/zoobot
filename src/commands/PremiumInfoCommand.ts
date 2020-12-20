75c870b1fc325d8f3c4a772b9f4fc11423d6b6d6
Ian <ianthomasevistoN@gmail.com>
2020-12-20 16:44:15 -0500

    Punctuation fix

--- src/commands/PremiumInfoCommand.ts
+++ src/commands/PremiumInfoCommand.ts
@@ -29,7 +29,7 @@ class PremiumInfoCommand extends Command {
         embed.addField("Free encounter max stack: **x3**", "Save up to **15** free encounters at a time, wasting less when you can't play.", true)
         embed.addField("Capture reset rate: **x2**", "Twice the captures, from 1 every 4 hours to **1 every 2 hours**. **12 per day** if you use them all!");
         embed.addField("Xp boost reset rate: **x2**", "Twice the xp boosts, from 1 every hour to **1 every 30 minutes**. Boost your animals to the max!", true);
-        embed.addField("Crew animals gain xp for __every__ message you send.", "Want a reward for being extra active in your server? This is it.", true);
+        embed.addField("Crew animals gain xp for __every__ message you send", "Want a reward for being extra active in your server? This is it.", true);
         embed.addField("*Support the creator!*", "Hey, this all takes a lot of effort to make, not to mention the fees for keeping it up! If you help me out in this way, I'd really appreciate it. :)");
 
         embed.setFooter("Pretty please?");
