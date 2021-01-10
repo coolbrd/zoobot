881f14a4d33bdd0dc7a5f922505d4bcfa6a96444
Ian <ianthomasevistoN@gmail.com>
2021-01-10 14:06:44 -0500

    Added vote message

--- src/commands/EncounterCommand.ts
+++ src/commands/EncounterCommand.ts
@@ -7,6 +7,7 @@ import CommandReceipt from "../structures/Command/CommandReceipt";
 import gameConfig from "../config/gameConfig";
 import BeastiaryClient from "../bot/BeastiaryClient";
 import { inspect } from "util";
+import VoteCommand from "./VoteCommand";
 
 class EncounterCommand extends GuildCommand {
     public readonly names = ["encounter", "e"];
@@ -27,9 +28,11 @@ class EncounterCommand extends GuildCommand {
         if (player.encountersLeft <= 0) {
             let noEncountersLeftString = `You don't have any encounters left. Next encounter reset: **${remainingTimeString(player.freeEncounters.nextReset)}**.`;
 
+            noEncountersLeftString += "\n\nWant more?"
             if (!player.getPremium()) {
-                noEncountersLeftString += `\n\nWant more? Subscribe at <${gameConfig.patreonLink}> for exclusive premium features such as more encounters, captures, and xp!`;
+                noEncountersLeftString += `\nSubscribe at <${gameConfig.patreonLink}> for exclusive premium features such as more encounters, captures, and xp!`;
             }
+            noEncountersLeftString += `\nUse the \`${VoteCommand.primaryName}\` command to see all the places where you can vote for The Beastiary in exchange for more encounters.`;
 
             betterSend(parsedMessage.channel, noEncountersLeftString);
             
