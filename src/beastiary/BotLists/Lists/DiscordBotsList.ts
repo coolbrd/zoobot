06122661506898fbe32817dd1af752b7f6217d60
Ian <ianthomasevistoN@gmail.com>
2021-01-26 13:08:44 -0500

    Fixed endpoint typo

--- src/beastiary/BotLists/Lists/DiscordBotsList.ts
+++ src/beastiary/BotLists/Lists/DiscordBotsList.ts
@@ -2,7 +2,7 @@ import { DISCORD_BOATS_TOKEN } from "../../../config/secrets";
 import BotList from "../BotList";
 
 export default class DiscordBotsList extends BotList {
-    protected readonly APIpath = "https://discord.bots.gg/api/vi/bots/:id/stats";
+    protected readonly APIpath = "https://discord.bots.gg/api/v1/bots/:id/stats";
     protected readonly APItoken = DISCORD_BOATS_TOKEN;
     protected readonly guildCountPropertyName = "guildCount";
     protected readonly shardCountPropertyName = "shardCount"
